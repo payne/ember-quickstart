890517628434e379e86ce4df237fcfc58493ce79
Matt Payne <Payne@MattPayne.org>
2016-08-20 16:45:33 -0500

    rootURL allows app to be served from http://domain.com/test2

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-quickstart',
     environment: environment,
-    rootURL: '/',
+    rootURL: '/test2',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
