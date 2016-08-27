ebaefc35cb2a09850d4051682e8dbd3ed5ed17ba
Matt Payne <Payne@MattPayne.org>
2016-08-27 13:24:16 -0500

    rootURL set to /

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+
   model() {
     return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann', 'John Lennon', 'Paul','George','Ringo'];
   }
--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-quickstart',
     environment: environment,
-    rootURL: '/test2',
+    rootURL: '/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
